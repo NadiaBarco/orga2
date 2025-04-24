f4e95241dcb9689ba0cdf0314a45df23a5905962
NADIA <barconadia38@gmail.com>
2025-04-24 16:05:02 +0000

    subida de archivo asm

---
+++ 2022C1R/solucion/ej1.asm
@@ -0,0 +1,85 @@
+;########### LISTA DE FUNCIONES IMPORTADAS
+extern strPrint
+extern strDelete
+extern strClone
+
+extern malloc
+extern free
+extern fprintf
+
+global countVowels
+global createLettersQuantityArray
+global getMaxVowels
+;########### SECCION DE DATOS
+section .data
+OFFSET_CONSONANTS_QTY EQU 0
+OFFSET_WORD EQU 8
+OFFSET_VOWELS_QTY EQU 16
+
+OFFSET_LETTERS_QUANTITY EQU 24
+;########### SECCION DE TEXTO (PROGRAMA)
+section .text
+
+
+%define LQ_SIZE 24
+
+%define LETTER_A 0x61
+%define LETTER_E 0x65
+%define LETTER_I 0x69
+%define LETTER_O 0x6F
+%define LETTER_U 0x75
+; rdi, rsi, rdx, rcx, r8, and r9.
+
+; uint8_t countVowels(char* word);
+countVowels:
+    push rbp
+    mov rbp,rsp
+    xor al,al
+    .ciclo:
+        mov dl, byte[rdi]
+        cmp dl, 0
+        je .fin
+
+        cmp dl, LETTER_A      ; ¿Es 'a'?
+        je .es_vocal
+        cmp dl, LETTER_E      ; ¿Es 'e'?
+        je .es_vocal
+        cmp dl, LETTER_I      ; ¿Es 'i'?
+        je .es_vocal
+        cmp dl, LETTER_O      ; ¿Es 'o'?
+        je .es_vocal
+        cmp dl, LETTER_U      ; ¿Es 'u'?
+        je .es_vocal
+
+        jmp .sigo             ; No es vocal, continuar
+        
+    .es_vocal:
+        inc al                ; Incrementar contador de vocales
+
+    .sigo:
+        inc rdi
+        jmp .ciclo
+        
+
+.fin:
+    pop rbp
+    ret
+
+
+;letters_quantity_t* createLettersQuantityArray(uint8_t size);
+createLettersQuantityArray:
+    push rbp
+    mov rbp,rsp
+ 
+    movzx rdi, dil 
+    mov rax, OFFSET_LETTERS_QUANTITY
+    mul rdi
+    mov rdi, rax
+    call malloc 
+
+    pop rbp
+    ret
+
+; char* getMaxVowels(letters_quantity_t* wq_array, uint8_t array_size);
+getMaxVowels:
+    ; COMPLETAR
---
+++ 2022C2PT1/solucion/ej1/ej1.asm
@@ -0,0 +1,16 @@
+global agrupar
+
+
+;########### SECCION DE DATOS
+section .data
+
+;########### SECCION DE TEXTO (PROGRAMA)
+section .text
+
+;char** agrupar(msg_t *msgArr, size_t msgArr_len);
+agrupar:
+    push rbp
+    mov rbp,rsp
+
+    pop rbp
+
