6dd71b1e00a41320d32e9628bbd33951eab1a005
NADIA <barconadia38@gmail.com>
2025-04-26 22:37:49 +0000

    fix llamados a malloc

--- 2022C1R/solucion/ej1.asm
+++ 2022C1R/solucion/ej1.asm
@@ -34,7 +34,7 @@ section .text
 countVowels:
     push rbp
     mov rbp,rsp
-    xor al,al
+    xor rax,rax
     .ciclo:
         mov dl, byte[rdi]
         cmp dl, 0
@@ -54,7 +54,7 @@ countVowels:
         jmp .sigo             ; No es vocal, continuar
         
     .es_vocal:
-        inc al                ; Incrementar contador de vocales
+        inc rax               ; Incrementar contador de vocales
 
     .sigo:
         inc rdi
@@ -71,12 +71,12 @@ createLettersQuantityArray:
     push rbp
     mov rbp,rsp
     sub rsp,16
-    mov byte[rbp-8], dil
+
     push r12
 
     movzx r12, dil
     mov rax, OFFSET_LETTERS_QUANTITY
-    mul dil
+    mul r12
     mov rdi, rax
 
     call malloc 
@@ -101,66 +101,67 @@ createLettersQuantityArray:
         jmp .ciclo
 
 
-    .fin:
-        mov rax, [rbp-16]
-        pop r12
-        add rsp, 16
-        pop rbp
-        ret
+ .fin:
+    mov rax, [rbp-16]
+    pop r12
+    add rsp, 16
+    pop rbp
+    ret
 
 ; char* getMaxVowels(letters_quantity_t* wq_array, uint8_t array_size);
 ;rdi= *wq_array
 ;sil = array_size
 getMaxVowels:
-    push rbp
-    mov rbp,rsp
-    sub rsp,16
-    push r11
-    push r12
+;    push rbp
+;    mov rbp,rsp
+;    sub rsp,16
+;    push r11
+;    push r12
 
-    xor r12,r12
-    xor r11,r11
+;    xor r12,r12
+;    xor r11,r11
 
-    movzx r9, sil 
+;    movzx r9, sil 
 
-    mov r11, rdi          ; r11= estoy en el struc
-    cmp r11, 0              ;Es un puntero nulo?
-    je .fin                 ; Si lo es no hago nada
+;    mov r11, rdi          ; r11= estoy en el struc
+;    cmp r11, 0              ;Es un puntero nulo?
+;    je .fin                 ; Si lo es no hago nada
     
-    mov [rbp-8],rdi
-    add r11, [r11+OFFSET_VOWELS_QTY] ; accedemos al vowels_qty
+;    mov [rbp-8],rdi
+;    add r11, [r11+OFFSET_VOWELS_QTY] ; accedemos al vowels_qty
     
     ;Tomamos el prox elem a comparar
-    add rdi, OFFSET_LETTERS_QUANTITY
-    mov r12, [rdi + OFFSET_VOWELS_QTY]
+;    add rdi, OFFSET_LETTERS_QUANTITY
+;    mov r12, [rdi + OFFSET_VOWELS_QTY]
 
-    sub r9,2
-    .ciclo:
-        cmp r9, 0           ;Llegue al final de arreglo?
-        je .fin 
+;    sub r9,2
+;    .ciclo:
+;        cmp r9, 0           ;Llegue al final de arreglo?
+;        je .fin 
 
-        cmp r11, r12
-        jle .esMenor
+;        cmp r11, r12
+;        jle .esMenor
 
         ;mov [rbp-8], rdi     ;Puntero a la estructura
-        add rdi, OFFSET_LETTERS_QUANTITY
-        movzx r12, byte[rdi + OFFSET_VOWELS_QTY]
-        dec r9
-        jmp .ciclo
+;        add rdi, OFFSET_LETTERS_QUANTITY
+;        movzx r12, byte[rdi + OFFSET_VOWELS_QTY]
+;        dec r9
+;        jmp .ciclo
 
-        .esMenor:
-            mov r11,r12
-            dec r9
-            ;add rdi, OFFSET_LETTERS_QUANTITY
-            mov [rbp-8], rdi
-            movzx r12, byte[rdi + OFFSET_VOWELS_QTY]
-            jmp .ciclo
+;        .esMenor:
+;            mov r11,r12
+;            dec r9
+;            ;add rdi, OFFSET_LETTERS_QUANTITY
+;            mov [rbp-8], rdi
+;            movzx r12, byte[rdi + OFFSET_VOWELS_QTY]
+;            jmp .ciclo
 
 
- .fin:
-    mov rax, [rbp-8]
-    add rsp,16
-    pop r12
-    pop r11
-    pop rbp
+; .fin:
+;    mov rax, [rbp-8]
+;    add rsp,16
+;    pop r12
+;    pop r11
+;    pop rbp
+;    ret
     
