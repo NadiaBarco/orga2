711a4c192f0355e8cd8980f45f66c6210924e5f5
NADIA <barconadia38@gmail.com>
2025-04-29 23:48:44 +0000

    Error 1b-templos

--- 2023C1P/solucion/ej1/ej1.asm
+++ 2023C1P/solucion/ej1/ej1.asm
@@ -15,27 +15,28 @@ templosClasicos:
     push rbp
     mov rbp, rsp
     push r13
-    push r12    ;r12=contador 
+    push r12    ;r12=   puntero al nuevo array
     push r11    ; r11 puntero al nuevo array
-    push r14
+    push r14    
     push r15
-    
-    push rbx
+    push rbx    ; Puntero al array de templos
 
     mov rbx, rdi
-    call cuantosTemplosClasicos
-
-    mov r12d, eax           ; r12d = cant de templos clasicos 
-
-    ;Armamos el array de templos_clasicos
-    mov edi, r12d       
+    mov rcx, rsi
+    call cuantosTemplosClasicos     ; rax = cant de templos clasicos
+    ;Armamos el tamaño de nuevo array de templos_clasicos
+    mov r9d,eax
+    mov r8d, OFFSET_TEMPLO    
     ; multiplicamos por el tamaño de la estructura
-    mul edi, OFFSET_TEMPLO
-    call malloc 
+    mul r8d
+    
+    mov rdi, rax
+    call malloc             ; Tenemos el puntero al nuevo array 
+    mov r12, rax
     mov r11,rax             ; r11 =puntero al nuevo array 
 
     .ciclo:
-        cmp r12d, 0         ; Quedan templos por recorrer?
+        cmp r9d, 0         ; Quedan templos por recorrer?
         je .fin
 
         movzx r13, byte[rbx + OFFSET_COLUMN_CORTO]
@@ -44,17 +45,42 @@ templosClasicos:
         shl r13, 1
         add r13, 1
 
-        cmp r13, r14
+        cmp r13, r14        ; 2 * N + 1 = M
         jne .siguiente
 
-        ; Agrego el templo clasico
-        
-        
-         
+        ; Agrego el templo clasico al nuevo array
+        mov r15b, [rbx + OFFSET_COLUMN_CORTO]       ; r15b = temploArr[rbx]->column_corto
+        mov [r11 + OFFSET_COLUMN_CORTO], r15b       ; newArray[r11]->colum_corto = r15b
 
+        mov r15b, [rbx + OFFSET_COLUMN_LARGO]     
+        mov [r11 + OFFSET_COLUMN_LARGO], r15b
 
+        ; Reservamos memoria para el nombre    
+        mov rdi, [rbx + OFFSET_NOMBRE]         ; rdi= puntero a char a clonar
+        call strClone                         ; rax = puntero al string 
 
+        mov [r11 + OFFSET_NOMBRE], rax
 
+        ;Vamos al siguiente templo
+
+        add r11, OFFSET_TEMPLO
+        add rbx, OFFSET_TEMPLO
+        dec r9d
+        jmp .ciclo
+
+        .siguiente:
+            add r11, OFFSET_TEMPLO
+            add rbx, OFFSET_TEMPLO
+
+            mov rdi, rbx
+            cmp rdi, 0 
+            je .fin
+
+            dec r9d
+            jmp .ciclo
+
+ .fin:
+    mov rax, r12
     pop rbx
     pop r15
     pop r14
@@ -76,46 +102,125 @@ mov rbp, rsp
 push r12
 push r13
 push rbx
-sub rsp, 8  ; ALINEADA
+push r14  ; ALINEADA
 xor rax,rax
 mov rbx, rdi
-
-; RECORRO EL ARRAY
+mov r14, rsi
+; RECORRO EL ARRAY de TEMPLOS
 .ciclo:
     ; el puntero al array es nulo?
     cmp rbx, 0
     je .fin
 
-
+    
     ; llegamos al final de la lista?
-    cmp rsi, 0      
+    cmp r14, 0      
     je .fin
 
     ;verifico que sea un templo clasico
     movzx r12, byte[rbx + OFFSET_COLUMN_CORTO]    ; cargo N columna corta
     movzx r13, byte[rbx + OFFSET_COLUMN_LARGO]    ; cargo M columna larga
 
-    ; Templo Clasico
+    
     shl r12, 1
-    add r12 ,1
+    add r12 ,1          ; Templo Clasico 2*N + 1
 
-    cmp r12, r13
+    cmp r12, r13        ; 2*N + 1 = M
     jne .siguiente
+
+
 
     add eax,1
     add rbx, OFFSET_TEMPLO
-    dec rsi
+    dec r14
     jmp .ciclo
 
     .siguiente:
         add rbx, OFFSET_TEMPLO
-        dec rsi
+        dec r14
         jmp .ciclo 
 
 .fin:
-    add rsp,8
+    pop r14
     pop rbx
     pop r13
     pop r12
     pop rbp
     ret
+
+
+
+; char* strClone(char* a)
+strClone:
+ push rbp
+ mov rbp, rsp
+ push r12						; contador
+ push r13						; r13 = len del string 
+ push r14						; Muevo el tope de la pila para guardar rdi y 8 bytes + para alinear
+ push r15						; puntero a la nueva memoria
+ push rbx
+ sub rsp,8						; ALINEADO	
+ mov r14, rdi					; rbx= el puntero del string 
+
+ ; rdi sigue siendo el puntero
+ call strLen						; Obteniene la longitud del string 
+ mov r13d, eax							; preserva rax, contiene la longitud del  string 
+ add r13d, 1							; Sumamos el caracter nulo
+ 
+ ;Calcula el espacio que tendra la memoria 
+ mov edi, r13d						; Son bytes entonces long(a)=espacio_de_mem_en_bytes
+ call malloc						; rax= puntero se encuentra 
+ 
+
+ mov r15, rax						; r15 = puntero a la nueva mem
+ mov rbx, rax
+; Copia byte a byte
+ .ciclo:
+	cmp r13, 0
+	je .fin
+
+	mov r12b, byte[r14]
+	mov byte[r15], r12b
+	inc r14
+	inc r15
+	dec r13
+	jmp .ciclo
+
+ .fin:
+	mov rax, rbx
+	add rsp,8
+	pop rbx
+ 	pop r15
+	pop r14
+	pop r13
+	pop r12
+ 	pop rbp
+	ret
+
+
+; uint32_t strLen(char* a)
+strLen:
+ push rbp
+ mov rbp, rsp
+ push r12
+ sub rsp, 8
+ xor eax, eax			; inicializo contador
+ mov r12, rdi
+
+
+
+ .ciclo:
+	; Accede rdi al primer caracter
+	cmp byte[r12], 0		; Es igual a NULL(0)?
+	je .fin					; Si es igual, termino
+
+	inc eax					; incremento el contador
+	inc r12					; Incrementa en un Byte, el puntero al proximo caracter
+
+	jmp .ciclo
+	 
+ .fin:
+	add rsp, 8
+ 	pop r12
+ 	pop rbp
+	ret
